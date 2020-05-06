31431944bc9461f5885064a8a614ee4e81b8a817
Perkele95 <49433241+Perkele95@users.noreply.github.com>
2020-05-06 11:41:30 +0200

    eps_main.c working build
    The code should be free of errors after compilation in atmel studio 7.0.

--- eps_main.c
+++ eps_main.c
@@ -1,14 +1,13 @@
  /* EPS main made for ATtiny45.
    Continuously monitors voltage and current, both intput and output.
-   PWM is set based on what mode the system is in. */
+   This build has been compiled with atmel studio 7.0. */
 
 // ------- Preamble -------- //
+#define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#include <math.h>                           /* for round() and floor() */
 
-#define F_CPU 16000000UL
 /* Note: REF_VCC is the internal voltage. */
 #define REF_VCC 1.1
 #define VOLTAGE_DIV_FACTOR  6      /* measured division by voltage divider */
@@ -17,11 +16,26 @@
 #define VCHARGE 7.2
 #define IBAT_THRESHOLD 0.4375  /* 5% of the discharge rate of 4 batteries, or (35/20)/4 */ 
 
+// ------ variables ------ //
+volatile float voltageIn_a;
+volatile float voltageIn_b;
+volatile float currVoltIn;        /*  currVolt is voltage measured after Rs  */
+volatile float currentIn_a;
+volatile float currentIn_b;
+
+volatile float voltageOut;
+volatile float currVoltOut;
+volatile float currentOut;
+
+float d_u;
+float d_i;
+uint8_t chargeFlag;
+
 // -------- Functions --------- //
 void initPWM(void){
     // PLL allows for internal 64 MHz clock for timer1
     PLLCSR = (1 << PLLE);                   /*  Enable PLL for timer1 */
-    _delay_ms(0.1ms);                     /*  Wait for PPL to stabilize */
+    _delay_ms(0.1);                     /*  Wait for PPL to stabilize */
     while(!(PLLCSR & (1 << PLOCK)));            /* PLL Lock Detector  */
     PLLCSR = (1 << PCKE);                           /* PCK Enable */
     
@@ -37,6 +51,13 @@ void initADC(void) {
   ADCSRA |= ( 1 << ADIE);                            /*  enable interrupt    */
   ADCSRA |= (1 << ADPS1) | (1 << ADPS2);    /* ADC clock prescaler /64 */
   ADCSRA |= (1 << ADEN);                              /* enable ADC */
+}
+
+// This selects the next output pin based on currentChannel
+void selectADCchannel(uint8_t channel){
+	//0xE0 is 11100000
+	//0x1F is 00011111
+	ADMUX = (ADMUX & 0xE0) | (channel & 0x1F);
 }
 
 ISR(ADC_vect){
@@ -69,19 +90,12 @@ ISR(ADC_vect){
     }
 }
 
-// This selects the next output pin based on currentChannel
-void selectADCchannel(uint8_t channel){
-    //0xE0 is 11100000
-    //0x1F is 00011111
-    ADMUX = (ADMUX & 0xE0) | (channel & 0x1F);
-}
-
 // MPPT function sets new duty cycle
 void MPPT(void){
   d_u = voltageIn_b - voltageIn_a;
   d_i = currentIn_b - currentIn_a;
-  if(d_u = 0){
-    if(d_i = 0){
+  if(d_u == 0){
+    if(d_i == 0){
         // do nothing
       } else {
         if(d_i > 0){
@@ -91,8 +105,8 @@ void MPPT(void){
         }
       }
   } else {
-    if((d_i/d_u) = (-(currentIn_b/voltageIn_b))){
-      return D;
+    if((d_i/d_u) == (-(currentIn_b/voltageIn_b))){
+      // do nothing
     } else {
       if((d_i/d_u) > (-(currentIn_b/voltageIn_b))){
         OCR1A -= 1;
@@ -112,7 +126,7 @@ void cvMode(void){
       OCR1A -= 1;
     }
     // Sample adc
-    for (i = 0; i < 3; i++) {
+    for (uint8_t k = 0; k < 3; k++) {
       ADCSRA |= (1 << ADSC);
       while (ADCSRA & (1 << ADSC)){}
     }
@@ -121,35 +135,20 @@ void cvMode(void){
 }
 
 int main(void) {
-  // ------ variables ------ //
-  volatile float voltageIn_a;
-  volatile float voltageIn_b;
-  volatile float currVoltIn;        /*  currVolt is voltage measured after Rs  */
-  volatile float currentIn_a;
-  volatile float currentIn_b;
-  
-  volatile float voltageOut;
-  volatile float currVoltOut;
-  volatile float currentOut;
-  
-  float d_u;
-  float d_i:
-  int chargeFlag;
-
   // -------- Inits --------- //
   initPWM(); 
   sei();
   initADC();
   _delay_ms(100);    /* Wait for converter to stabilise */
   
-  for (i = 0; i < 3; i++) {             /*  sample adc once to set "a" values to a non-zero value */
+  for (uint8_t i = 0; i < 3; i++) {             /*  sample adc once to set "a" values to a non-zero value */
       ADCSRA |= (1 << ADSC);                  /*  start conversion  */
       while (ADCSRA & (1 << ADSC)){}
   }
   
   // ------ Event loop ------ //
   while (1) {
-    for (i = 0; i < 3; i++) {             /*  adc sample */
+    for (uint8_t j = 0; j < 3; j++) {             /*  adc sample */
         ADCSRA |= (1 << ADSC);
         while (ADCSRA & (1 << ADSC)){}
     }
@@ -157,7 +156,7 @@ int main(void) {
     // First check is battery is being charged or not.
     // If so, check the current state, whether CC, CV or charge done.
     if(chargeFlag){
-        if(voltageOut_b >= VCHARGE){
+        if(voltageOut >= VCHARGE){
           cvMode(); // This is CV mode
         } else {
           MPPT();   // This is CC mode
@@ -166,7 +165,7 @@ int main(void) {
     // If not charging, check if charging needs to start because of low voltage.
     // Start MPPT regardless
     else{
-        if(voltageOut_b < VBAT_LOW){
+        if(voltageOut < VBAT_LOW){
           chargeFlag = 1;
           MPPT();
         } else {
@@ -175,5 +174,5 @@ int main(void) {
     }
     _delay_ms(200);
   }
-  return 0;
+return 0;
 }
